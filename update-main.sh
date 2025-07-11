5995bdd8f163e1428cf0f5bceac0ab223590739c
Dean Wampler <dean.wampler@ibm.com>
2025-07-11 13:27:32 -0400

    Refinements
    Signed-off-by: Dean Wampler <dean.wampler@ibm.com>

--- update-main.sh
+++ update-main.sh
@@ -3,13 +3,14 @@
 # A simple script that merges the latest work from "latest" to "main",
 # pushes the updates upstream, then switches to "latest" again.
 # This is mostly useful if you tend to work in "latest", so updates are
-# published immediately, and you periodically want to update main.
+# published immediately, and you periodically want to update "main".
 #------------------------------------------------------------------------
 
 git checkout latest
 git pull
+git push  # make sure latest has been pushed upstream
 git checkout main
 git pull
 git merge latest
 git push
-git checkout latest
+git checkout latest # end with latest checked out
